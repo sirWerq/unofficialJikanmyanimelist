19f2a156ef660ee37e10c54ae892a31cd7adcc2e
sirWerq <rifqx1904@gmail.com>
2023-12-03 20:47:21 +0700

    menambahkan fitur pagination

--- script.js
+++ script.js
@@ -1,22 +1,116 @@
 const listAnime = document.querySelector("#animeList")
 const input = document.querySelector('.inputUser')
 const btn = document.querySelector('.buttonInput')
+const paginationButton = document.querySelector('#pagination')
+
 
 getTampilan()
 
 async function getTampilan() {
     try {
-        const api = await fetch ("https://api.jikan.moe/v4/top/anime?limit=18")
-        const jsonAnime = await api.json()
-        const anime = jsonAnime.data
+        page = getParams()
+        const anime = await getData(page)
         let listCard = ''
-        anime.forEach(e => listCard += data(e))
+        if(Array.isArray(anime)) {
+            anime.forEach(e => listCard += data(e))
+        } else {
+            listCard += anime
+        }
         listAnime.innerHTML = listCard
+        let pageButton = await buttonPage()
+        paginationButton.innerHTML = pageButton
     }
     catch (err) {
         alert(err)
     }
 }
+
+async function getData(page = 1, target = "data") {
+    const api = await fetch(`https://api.jikan.moe/v4/top/anime?limit=18&page=${page}`)
+    const dataAnime = await api.json()
+    const dataTarget = dataAnime[`${target}`]
+    if (dataTarget.length == 0) {
+        return '<h1 class="text-center">DATA NOT FOUND</h1>'
+    }
+    return dataTarget
+}
+
+function getParams(parameter = "page") {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get(parameter) ? urlParams.get(parameter) : 1
+    return parseInt(page)
+}
+
+async function buttonPage() {
+    let url = getParams()
+    const pagination = await getData(url, "pagination")
+    if (url > pagination.last_visible_page) {
+        url = 1;
+    }
+    const buttonPrevious = url == 1 ? `<ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+    <a class="page-link">Previous</a>
+    </li>` : `
+    <ul class="pagination justify-content-center">
+    <li class="page-item">
+    <a class="page-link" href="?page=${url - 1}">Previous</a>
+    </li>
+    `
+    const pageNumber = dataPage(url, pagination.last_visible_page)
+    const buttonNext = pagination.has_next_page ? `<li class="page-item">
+    <a class="page-link" href="?page=${url + 1}">Next</a>
+    </li>` : `<li class="page-item disabled">
+    <a class="page-link">Next</a>
+    </li>`
+    
+    return buttonPrevious + pageNumber + buttonNext
+    
+}
+
+function dataPage(currPage, lastpage) {
+    const basePage = Math.floor(currPage / 4) * 4
+    const currPosition = currPage % 4
+    let text = ''
+    if (currPage < 4) {
+        for (let i = 1; i < 5; i++) {
+            if (i == currPosition) {
+                text += `<li class="page-item disabled"><a class="page-link" href="?page=${basePage + i}">${basePage + i}</a></li>`
+            } else {
+                text += `<li class="page-item"><a class="page-link" href="?page=${basePage + i}">${basePage + i}</a></li>`
+            }
+        }
+        text += `
+        <li class="page-item disabled"><a class="page-link" href="#">...</a></li>
+        <li class="page-item"><a class="page-link" href="?page=${lastpage}">${lastpage}</a></li>
+        `
+
+    } else if (currPage > lastpage - 4) {
+        text += `<li class="page-item"><a class="page-link" href="?page=1">1</a></li>
+        <li class="page-item disabled"><a class="page-link" href="#">...</a></li>`
+        for (let i = 4; i >= 0; i--) {
+            if (lastpage - i == currPage) {
+                text += `<li class="page-item disabled"><a class="page-link" href="?page=${lastpage - i}">${lastpage - i}</a></li>`
+            } else {
+                text += `<li class="page-item"><a class="page-link" href="?page=${lastpage - i}">${lastpage - i}</a></li>`
+            }
+        }
+    } else {
+        text += `<li class="page-item"><a class="page-link" href="?page=1">1</a></li>
+        <li class="page-item disabled"><a class="page-link" href="#">...</a></li>`
+        for (let i = -1; i < 5; i++) {
+            if (i == currPosition) {
+                text += `<li class="page-item disabled"><a class="page-link" href="?page=${basePage + i}">${basePage + i}</a></li>`
+            } else {
+                text += `<li class="page-item"><a class="page-link" href="?page=${basePage + i}">${basePage + i}</a></li>`
+            }
+        }
+        text += `<li class="page-item disabled"><a class="page-link" href="#">...</a></li>
+        <li class="page-item"><a class="page-link" href="?page=${lastpage}">${lastpage}</a></li>`
+
+    }
+    return text
+}
+
 
 btn.addEventListener('click', async function () {
     try {
@@ -30,7 +124,7 @@ btn.addEventListener('click', async function () {
             let listCard = ''
             anime.forEach(e => listCard += data(e))
             listAnime.innerHTML = listCard
-    }
+        }
     }
     catch (err) {
         alert(err)
@@ -90,4 +184,4 @@ function data(value) {
     </div>
     </div>
     `
-}
+}   
